bc30c494a2a4841736dc4c3571c75aaf153f2b87
Lina Parren <54841398+LinaParren@users.noreply.github.com>
2022-03-04 11:23:11 +0100

    Nieuwste versie pushen

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const express = require('express');
+const express = require('express')
+const { engine } = require('express-handlebars');
 const app = express();
-const port = 3000
 
-app.listen(port, () => {
-    console.log('port 3000')
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
+app.set("views", "./views");
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
+
+app.get('/zoeken', (req, res) => {
+  res.render('zoeken');
+});
+
+app.get('/verkennen', (req, res) => {
+  res.render('verkennen');
+});
+
+app.listen(3000);
