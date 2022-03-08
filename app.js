7eb0c7be0461e444ae2e219a64cff8f6ed9afe1a
Lina Parren <lina3@hotmail.nl>
2022-03-08 11:44:14 +0100

    MongoDB aanmaken

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars');
 const app = express();
 const path = require('path')
+const uri = process.env.MONGODB_URI;
 
 const port = process.env.PORT || 5000
 
